85a74ecfec06a1e1fb741cb3430261c5a08aa4de
pramebt <bdforwk@gmail.com>
2025-07-09 00:04:39 +0700

    Add OGL dependency and integrate Aurora component in Weatherpage for enhanced visual effects

--- src/pages/Weatherpage.jsx
+++ src/pages/Weatherpage.jsx
@@ -1,12 +1,20 @@
-import React from 'react'
-import Weather from '../components/Weather'
+import React from "react";
+import Weather from "../components/Weather";
+import Aurora from "../components/Aurora";
 
 const Weatherpage = () => {
   return (
     <div className="w-full h-screen flex items-center justify-center  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600">
-        <Weather/>
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={1}
+        amplitude={1.0}
+        speed={0.5}
+      >
+        <Weather />
+      </Aurora>
     </div>
-  )
-}
+  );
+};
 
-export default Weatherpage
+export default Weatherpage;
